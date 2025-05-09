53f77c6f11130121aa723dd7e8a59ea34fa968ba
Maram suliman <mms.bur@outlook.com>
2025-05-09 17:45:42 +0400

    Add files via upload

---
+++ helperfunction/HelperFunctions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    public class HelperFunctions
+    {
+
+        public static void Printter()
+        {
+            for (int i = 0; i < 5; i++)
+
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}   
---
+++ helperfunction/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelperFunctions.Printter();
+        }
+
+    }
+}
